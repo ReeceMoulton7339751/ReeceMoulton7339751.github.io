d0e9bc7ee4e6345447b1ff9df53b23b7b51a112e
ReeceMoulton7339751 <rjm7339751@lpssonline.com>
2026-04-01 13:12:04 +0000

    done

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -15,44 +15,69 @@ var init = function (window) {
         window.opspark.game = {};
         var game = window.opspark.game;
         
-        ///////////////////
-        // PROGRAM SETUP //
-        ///////////////////
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM SETUP ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
+var circle
 
-
-
+var circles = [ ]
+      
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
 
+              physikz.addRandomVelocity(circle, canvas, 2, 2);
 
-        // TODO 3 : Call the drawCircle() function
+              view.addChild(circle);
 
+              circles.push(circle);
+            }
+       
 
+        // TODO 3 / 7 : Call the drawCircle() function 
 
-        // TODO 7 : Use a loop to create multiple circles
+        drawCircle()
 
+drawCircle()
 
+drawCircle()
 
+drawCircle()
 
-        ///////////////////
-        // PROGRAM LOGIC //
-        ///////////////////
+drawCircle()
+for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
+
+            drawCircle();
+
+        }
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM LOGIC ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         /* 
-        This Function is called 60 times/second, producing 60 frames/second.
+        This Function is called 60 times/second producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
         function update() {
-            // TODO 4 : Update the position of each circle using physikz.updatePosition()
+            // TODO 4 : Update the circle's position //
+for (var i = 0; i < circles.length; i++) {
 
-            
-            // TODO 5 : Call game.checkCirclePosition() on your circles
-           
+               physikz.updatePosition(circles[i]);
 
-            // TODO 8 / TODO 9 : Iterate over the array
+               game.checkCirclePosition(circles[i]);
+}
+                      
+            // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
+
+
+
+
+
+
+                       // TODO 9 : Iterate over the array
            
             
         }
@@ -62,17 +87,29 @@ var init = function (window) {
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
+
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
-            }
-            
+                         if(circle.x > canvas.width){
+  circle.x = 0
+}
+
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            else if (circle.x < 0) {
 
+                circle.x = canvas.width;
 
+            } else if (circle.y > canvas.height) {
+
+                circle.y = 0;
+
+            } else if (circle.y < 0) {
+
+                circle.y = canvas.height;
+
+            }
+           
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
@@ -92,7 +129,7 @@ var init = function (window) {
     }
 };
 
-// DO NOT REMOVE THIS CODE //////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
